fb935268a6f622224a516cbd8c7cea7c6e0cbad2
rasi10 <mind_traveller@hotmail.com>
2020-05-26 20:49:30 +0200

    Update api.js

--- rasi10-hotel-test-vue/server/api.js
+++ rasi10-hotel-test-vue/server/api.js
@@ -1,174 +1,236 @@
-const express = require('express')
-const router = express.Router()
-const { createToken, sleep, userByPassword, userByHeaderToken, nextId } = require('./helper')
-const data = require('./data')
+import router, { push } from '@/router'
+import xhr, { baseUrl } from './xhr'
+import Store, { ACTIONS } from '@/store'
 
-// Login endpoints
+export function checkLoggedIn(redirect = true) {
+	const loggedIn = Store.state.user
+	if (redirect && !loggedIn) {
+		push('/login')
+		return false
+	}
+	return loggedIn
+}
 
-router.post('/login', async (req, res) => {
-	const user = userByPassword(req.body)
-	await sleep(500) // Make user wait so emulate a slower connection
-	if (user) {
-		user.token = createToken()
-		res.send({
-			username: user.username,
-			token: user.token
+export function checkLoggedOut() {
+	if (Store.state.user) {
+		push('/')
+	}
+}
+
+export async function logout() {
+	try {
+		await xhr.post(`${baseUrl}/api/logout`, null, authHeaders())
+	} catch (err) {
+		// Just logout anyway
+	} finally {
+		forceLogout(false)
+	}
+}
+
+export async function login(username: string, password: string, redirect: string = '/') {
+	try {
+		const user = await xhr.post(`${baseUrl}/api/login`, {
+			username,
+			password
 		})
-	} else {
-		res.status(401).send({ error: 'Bad username or password' })
+		Store.commit(ACTIONS.SET_USER, user)
+		redirect = redirect && redirect.startsWith('/login') ? '/' : redirect
+		push(redirect || '/')
+	} catch (err) {
+		throw err
 	}
-})
+}
 
-router.post('/logout', (req, res) => {
-	const user = userByHeaderToken(req)
-	if (user) {
-		user.token = null
+function authHeaders() {
+	return {
+		'X-User-Auth': JSON.stringify(Store.state.user)
 	}
-	res.sendStatus(200)
-})
+}
 
-// Room endpoints
+function forceLogout(doRedirect = true) {
+	Store.commit(ACTIONS.RESET_STATE)
+	const redirect = doRedirect ? `?redirect=${router.currentRoute.path}` : ''
+	push(`/login${redirect}`)
+}
 
-router.get('/rooms', (req, res) => {
-	if (userByHeaderToken(req)) res.send(data.rooms)
-	else res.sendStatus(401)
-})
-router.get('/room/:id', (req, res) => {
-	const room = data.rooms.find((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && room) res.send(room)
-	else res.sendStatus(401)
-})
-router.post('/room/new', (req, res) => {
-	if (userByHeaderToken(req)) {
-		const room = req.body
-		room.id = nextId(data.rooms)
-		room.created = new Date()
-		room.floor = parseFloat(room.floor) || 0
-		room.number = parseFloat(room.number) || 0
-		room.price = parseFloat(room.price) || 0
-		data.rooms.push(room)
-		res.send(room)
-	} else res.sendStatus(401)
-})
-router.put('/room/:id', (req, res) => {
-	const index = data.rooms.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		req.body.floor = parseFloat(req.body.floor) || 0
-		req.body.number = parseFloat(req.body.number) || 0
-		req.body.price = parseFloat(req.body.price) || 0
-		res.send(data.rooms[index] = req.body)
-	} else res.sendStatus(401)
-})
-router.delete('/room/:id', (req, res) => {
-	const index = data.rooms.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		data.rooms.splice(index, 1)
-		res.send({ ok: true })
-	} else res.sendStatus(401)
-})
+function force404() {
+	push(`/404`)
+}
 
-// Client endpoints
+export async function getAll() {
+	try {
+		await Promise.all([ getRooms(), getClients(), getBills(), getReservations() ])
+		return true
+	} catch (err) {
+		forceLogout()
+		return false
+	}
+}
 
-router.get('/clients', (req, res) => {
-	if (userByHeaderToken(req)) res.send(data.clients)
-	else res.sendStatus(401)
-})
-router.get('/client/:id', (req, res) => {
-	const client = data.clients.find((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && client) res.send(client)
-	else res.sendStatus(401)
-})
-router.post('/client/new', (req, res) => {
-	if (userByHeaderToken(req)) {
-		const client = req.body
-		client.id = nextId(data.clients)
-		client.created = new Date()
-		data.clients.push(client)
-		res.send(client)
-	} else res.sendStatus(401)
-})
-router.put('/client/:id', (req, res) => {
-	const index = data.clients.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		res.send(data.clients[index] = req.body)
-	} else res.sendStatus(401)
-})
-router.delete('/client/:id', (req, res) => {
-	const index = data.clients.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		data.clients.splice(index, 1)
-		res.send({ ok: true })
-	} else res.sendStatus(401)
-})
+export async function getRooms() {
+	try {
+		const rooms = await xhr.get(`${baseUrl}/api/rooms`, authHeaders())
+		Store.commit(ACTIONS.SET_ROOMS, rooms)
+		return rooms
+	} catch (err) {
+		return forceLogout()
+	}
+}
 
-// Bill endpoints
+export async function createRoom(room) {
+	try {
+		return await xhr.post(`${baseUrl}/api/room/new`, room, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
 
-router.get('/bills', (req, res) => {
-	if (userByHeaderToken(req)) res.send(data.bills)
-	else res.sendStatus(401)
-})
-router.get('/bill/:id', (req, res) => {
-	const bill = data.bills.find((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && bill) res.send(bill)
-	else res.sendStatus(401)
-})
-router.post('/bill/new', (req, res) => {
-	if (userByHeaderToken(req)) {
-		const bill = req.body
-		bill.id = nextId(data.bills)
-		bill.created = new Date()
-		bill.value = parseFloat(bill.value) || 0
-		data.bills.push(bill)
-		res.send(bill)
-	} else res.sendStatus(401)
-})
-router.put('/bill/:id', (req, res) => {
-	const index = data.bills.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		req.body.value = parseFloat(req.body.value) || 0
-		res.send(data.bills[index] = req.body)
-	} else res.sendStatus(401)
-})
-router.delete('/bill/:id', (req, res) => {
-	const index = data.bills.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		data.bills.splice(index, 1)
-		res.send({ ok: true })
-	} else res.sendStatus(401)
-})
+export async function getRoom(id) {
+	try {
+		return await xhr.get(`${baseUrl}/api/room/${id}`, authHeaders())
+	} catch (err) {
+		return force404()
+	}
+}
 
-// Reservation endpoints
+export async function editRoom(id, room) {
+	try {
+		return await xhr.put(`${baseUrl}/api/room/${id}`, room, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
 
-router.get('/reservations', (req, res) => {
-	if (userByHeaderToken(req)) res.send(data.reservations)
-	else res.sendStatus(401)
-})
-router.get('/reservation/:id', (req, res) => {
-	const reservation = data.reservations.find((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && reservation) res.send(reservation)
-	else res.sendStatus(401)
-})
-router.post('/reservation/new', (req, res) => {
-	if (userByHeaderToken(req)) {
-		const reservation = req.body
-		reservation.id = nextId(data.reservations)
-		reservation.created = new Date()
-		data.reservations.push(reservation)
-		res.send(reservation)
-	} else res.sendStatus(401)
-})
-router.put('/reservation/:id', (req, res) => {
-	const index = data.reservations.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		res.send(data.reservations[index] = req.body)
-	} else res.sendStatus(401)
-})
-router.delete('/reservation/:id', (req, res) => {
-	const index = data.reservations.findIndex((c) => c.id === parseInt(req.params.id, 10))
-	if (userByHeaderToken(req) && index !== -1) {
-		data.reservations.splice(index, 1)
-		res.send({ ok: true })
-	} else res.sendStatus(401)
-})
+export async function deleteRoom(id) {
+	try {
+		return await xhr.delete(`${baseUrl}/api/room/${id}`, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
 
-module.exports = router
+export async function getClients() {
+	try {
+		const clients = await xhr.get(`${baseUrl}/api/clients`, authHeaders())
+		Store.commit(ACTIONS.SET_CLIENTS, clients)
+		return clients
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function createClient(client) {
+	try {
+		return await xhr.post(`${baseUrl}/api/client/new`, client, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function getClient(id) {
+	try {
+		return await xhr.get(`${baseUrl}/api/client/${id}`, authHeaders())
+	} catch (err) {
+		return force404()
+	}
+}
+
+export async function editClient(id, client) {
+	try {
+		return await xhr.put(`${baseUrl}/api/client/${id}`, client, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function deleteClient(id) {
+	try {
+		return await xhr.delete(`${baseUrl}/api/client/${id}`, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function getBills() {
+	try {
+		const bills = await xhr.get(`${baseUrl}/api/bills`, authHeaders())
+		Store.commit(ACTIONS.SET_BILLS, bills)
+		return bills
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function createBill(bill) {
+	try {
+		return await xhr.post(`${baseUrl}/api/bill/new`, bill, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function getBill(id) {
+	try {
+		return await xhr.get(`${baseUrl}/api/bill/${id}`, authHeaders())
+	} catch (err) {
+		return force404()
+	}
+}
+
+export async function editBill(id, bill) {
+	try {
+		return await xhr.put(`${baseUrl}/api/bill/${id}`, bill, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function deleteBill(id) {
+	try {
+		return await xhr.delete(`${baseUrl}/api/bill/${id}`, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function getReservations() {
+	try {
+		const reservations = await xhr.get(`${baseUrl}/api/reservations`, authHeaders())
+		Store.commit(ACTIONS.SET_RESERVATIONS, reservations)
+		return reservations
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function createReservation(reservation) {
+	try {
+		return await xhr.post(`${baseUrl}/api/reservation/new`, reservation, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function getReservation(id) {
+	try {
+		return await xhr.get(`${baseUrl}/api/reservation/${id}`, authHeaders())
+	} catch (err) {
+		return force404()
+	}
+}
+
+export async function editReservation(id, reservation) {
+	try {
+		return await xhr.put(`${baseUrl}/api/reservation/${id}`, reservation, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
+
+export async function deleteReservation(id) {
+	try {
+		return await xhr.delete(`${baseUrl}/api/reservation/${id}`, authHeaders())
+	} catch (err) {
+		return forceLogout()
+	}
+}
